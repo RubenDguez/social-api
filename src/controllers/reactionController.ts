a6147e8d78df0ac3d25d6b3f916666f003b63fca
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-10-04 23:11:10 -0400

    add: remove reaction

--- src/controllers/reactionController.ts
+++ src/controllers/reactionController.ts
@@ -11,11 +11,24 @@ export const addReaction = async (req: Request, res: Response) => {
 
     const thought = await Thought.findOneAndUpdate({ _id: thoughtId }, { $addToSet: { reactions: { reactionBody, username } } }, { runValidators: true, new: true });
 
-    if (!thought) {
-      return res.status(404).json({ message: 'No thought with that ID' });
-    }
+    if (!thought) return res.status(404).json({ message: 'No thought with that ID' });
 
     return res.status(201).json(thought);
+  } catch (error) {
+    const ERROR = error as Error;
+    return res.status(500).json(ERROR.message);
+  }
+};
+
+export const removeReaction = async (req: Request, res: Response) => {
+  const { thoughtId, reactionId } = req.params;
+
+  try {
+    const thought = await Thought.findOneAndUpdate({ _id: thoughtId }, { $pull: { reactions: { _id: reactionId } } }, { runValidators: true, new: true });
+
+    if (!thought) return res.status(404).json({ message: 'No thought with that ID' });
+
+    return res.status(200).json(thought);
   } catch (error) {
     const ERROR = error as Error;
     return res.status(500).json(ERROR.message);
--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
+import { addReaction, removeReaction } from '../../controllers/reactionController.js';
 import { addThought, deleteThought, getAll, getOne } from '../../controllers/thoughtController.js';
-import { addReaction } from '../../controllers/reactionController.js';
+
 const router = Router();
 
 router.post('/', addThought);
@@ -8,6 +9,7 @@ router.get('/:thoughtId', getOne);
 router.get('/', getAll);
 router.delete('/:thoughtId', deleteThought);
 
-router.post('/:thoughtId/reactions', addReaction)
+router.post('/:thoughtId/reactions', addReaction);
+router.delete('/:thoughtId/reactions/:reactionId', removeReaction);
 
 export default router;
